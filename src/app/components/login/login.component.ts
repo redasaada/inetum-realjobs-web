e01feb719f8b383fb0982f8db48f9005bb9df29f
redasaada <reda.saada@realdolmen.com>
2021-11-29 13:46:51 +0100

    validation done for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { User } from 'src/app/models/user.model';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
@@ -10,45 +9,58 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
-  form: FormGroup;
 
-  user: User ={
-    username: '',
-    password: ''
-  }
+  loginForm: FormGroup;
 
-  submitted=false;
-  constructor(private userService : AuthenticationService,
-    private router: Router) { 
-    this.form = new FormGroup({
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('',[Validators.required])
-    });
-  }
+  badCredentials: boolean = false;
+
+  // empty:boolean;
+
+  constructor(private userService: AuthenticationService,
+    private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+    this.loginForm.valueChanges.subscribe(() =>{
+      this.badCredentials=false;
+    });
   }
 
   login() {
-    this.userService.login(this.form.getRawValue()).subscribe(() => {
-      this.router.navigate(['']);
-    });
+    if (this.loginForm.valid) {
+      // this.empty=false;
+      this.userService.login(this.loginForm.getRawValue()).subscribe(() => {
+        this.router.navigate(['']);
+        this.badCredentials = false;
+      }, error => {
+        this.badCredentials = true;
+      });
+    } else {
+      // this.empty= true;
+    }
+
   }
 
-  logout(){
+  logout() {
     this.userService.logout();
   }
 
-  isLoggedIn(): boolean{
+  isLoggedIn(): boolean {
     return this.userService.isLoggedIn();
   }
 
-  newUser(): void{
-    this.submitted = false;
-    this.user = {
-      username: '',
-      password: ''
-    };
+  get form() {
+    return this.loginForm.controls;
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 }
---
+++ src/app/primeng-imports.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { ButtonModule } from 'primeng/button';
+import { InputText } from 'primeng/inputtext';
+import { Panel } from 'primeng/panel';
+
+@NgModule({
+    imports: [
+        ButtonModule,
+        InputText,
+        Panel
+    ],
+    declarations: []
+})
+export class PrimengImportsModule{}
