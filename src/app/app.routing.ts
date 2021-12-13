4fbb6324175b903f254e13a25df81c00720c1044
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-13 16:55:33 +0100

    Formatting

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,13 @@
-import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent as LoginComponent } from "./components/login/login.component";
-import { UserDetailsComponent } from "./components/user-details/user-details.component";
-import { FilterFormComponent } from "./components/filter-form/filter-form.component";
-import { AuthGuardService } from "./guard/authGuard.service";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent as LoginComponent} from "./components/login/login.component";
+import {UserDetailsComponent} from "./components/user-details/user-details.component";
+import {FilterFormComponent} from "./components/filter-form/filter-form.component";
+import {AuthGuardService} from "./guard/authGuard.service";
 
 const appRoutes: Routes = [
-    { path: 'users/:username', component: UserDetailsComponent, canActivate: [AuthGuardService]},
-    { path: 'login', component: LoginComponent},
-    { path: 'vacancy/search', component: FilterFormComponent}
+  {path: 'users/:username', component: UserDetailsComponent, canActivate: [AuthGuardService]},
+  {path: 'login', component: LoginComponent},
+  {path: 'vacancy/search', component: FilterFormComponent}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
