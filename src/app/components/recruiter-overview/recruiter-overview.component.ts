393d879477cd1d4ba22d43631dde803286685fbb
Johan Schoubben <Johan.Schoubben@inetum-realdolmen.world>
2021-12-15 13:39:46 +0100

    add component recruiter overview

---
+++ src/app/components/recruiter-overview/recruiter-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruiter-overview',
+  templateUrl: './recruiter-overview.component.html',
+  styleUrls: ['./recruiter-overview.component.scss']
+})
+export class RecruiterOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
