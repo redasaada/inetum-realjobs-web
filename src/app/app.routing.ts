d663613b721508104ee3d81ea0aa322d525b065c
Johan Schoubben <Johan.Schoubben@inetum-realdolmen.world>
2021-12-15 13:50:24 +0100

    add routing for recruiter overview page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,14 @@ import {UserDetailsComponent} from "./components/user-details/user-details.compo
 import {FilterFormComponent} from "./components/filter-form/filter-form.component";
 import {AuthGuardService} from "./guard/authGuard.service";
 import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {RecruiterOverviewComponent} from "./components/recruiter-overview/recruiter-overview.component";
 
 const appRoutes: Routes = [
   {path: "", redirectTo: "/vacancy/search", pathMatch: "full"},
   {path: 'users/:username', component: UserDetailsComponent, canActivate: [AuthGuardService]},
   {path: 'login', component: LoginComponent},
   {path: 'vacancy/search', component: FilterFormComponent},
+  {path: 'recruiter/overview', component: RecruiterOverviewComponent},
   {path: "**", component: NotFoundComponent},
 ];
 
