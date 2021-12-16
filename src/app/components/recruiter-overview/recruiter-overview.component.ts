6ac70962fb71945f41f3aa631f5f068b885dfdcf
Johan Schoubben <Johan.Schoubben@inetum-realdolmen.world>
2021-12-16 16:00:55 +0100

    refactor table on overview component

--- src/app/components/recruiter-overview/recruiter-overview.component.ts
+++ src/app/components/recruiter-overview/recruiter-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Vacancy} from "../../models/vacancy";
 import {VacancySearchService} from "../../services/vacancy-search.service";
 
@@ -10,11 +10,21 @@ import {VacancySearchService} from "../../services/vacancy-search.service";
 export class RecruiterOverviewComponent implements OnInit {
   vacancies: Vacancy[] = [];
   title: string = "Overview: My vacancies";
+  columns: any[];
+  defaultMessage: string = "Your vacancies will appear here." +
+    " Get started and post your first vacancy by clicking 'Post new vacancy'";
 
-  constructor(private vacancyService: VacancySearchService) { }
+  constructor(private vacancyService: VacancySearchService) {
+  }
 
   ngOnInit(): void {
     this.getAllVacancies();
+    this.columns = [
+      {field: 'functionTitle', header: 'Function title'},
+      {field: 'createdOn', header: 'Date posted'},
+      {field: 'createdBy', header: 'Vacancy manager'},
+      {field: 'applicants', header: 'Applicants'}
+    ];
   }
 
   getAllVacancies(): void {
